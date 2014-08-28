a76b3f9312c8911b5949e09ef4aba2ad267b2a3d
TheCowJr <josh.crazymajr@gmail.com>
2014-08-27 22:01:58 -0700

    Update GameState.java

--- com/theparkmc/simonsays/utils/GameState.java
+++ com/theparkmc/simonsays/utils/GameState.java
@@ -1,1 +1,25 @@
-//TODO: ADD CODE
+package com.theparkmc.simonsays.utils;
+
+public enum GameState {
+  IN_LOBBY(true), IN_GAME(false), POST_GAME(false), RESTARTING(false);
+  
+  private boolean canJoin;
+  
+  private static GameState currentState;
+  
+  GameState(boolean canJoin) {
+    this.canJoin = canJoin;
+  }
+  
+  public boolean canJoin() {
+    return canJoin;
+  }
+  
+  public static void setState(GameState state) {
+    return GameState.currentState == state;
+  }
+  
+  public static GameState getState() {
+    return currentState;
+  }
+}
