6248faa23010fdfbfff6cd9f91a27fec3bf412af
TheCowJr <josh.crazymajr@gmail.com>
2014-08-27 22:09:37 -0700

    Update ChatUtils.java

--- com/theparkmc/simonsays/utils/ChatUtils.java
+++ com/theparkmc/simonsays/utils/ChatUtils.java
@@ -1,1 +1,24 @@
-//TODO: Add Code
+package com.theparkmc.simonsays.utils;
+
+import static org.bukkit.ChatColor.*;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.player;
+
+public class ChatUtilities {
+  
+  public static void Broadcast(String msg) {
+    for (Player player : Bukkit.getOnlinePlayers()) {
+      player.sendMessage(starter() + msg);
+    }
+  }
+  
+  private static String starter() {
+    return GRAY + "[" + RED + "Simon-Says" + GRAY + "] ";
+  }
+  
+  public static void sendMessage(Player player, String msg) {
+    player.sendMessage(starter() + msg);
+  }
+  
+}
