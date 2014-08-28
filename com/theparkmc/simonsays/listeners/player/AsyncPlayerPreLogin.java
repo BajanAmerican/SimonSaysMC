9d6442a203f75168ce0ed848880d1a6b2c09c47a
TheCowJr <josh.crazymajr@gmail.com>
2014-08-27 19:33:50 -0700

    Update AsyncPlayerPreLogin.java

--- com/theparkmc/simonsays/listeners/player/AsyncPlayerPreLogin.java
+++ com/theparkmc/simonsays/listeners/player/AsyncPlayerPreLogin.java
@@ -1,1 +1,24 @@
-//TODO: Add Code
+package com.theparkmc.simonsays.listeners.player;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
+
+import com.theparkmc.simonsays.listeners.SSListeners;
+
+public class AsyncPlayerPreLogin extends SSListener {
+  
+  public AsyncPlayerPreLogin(SimonSays pl) {
+    super(pl);
+  }
+  
+  @EventHandler
+  public void playerPreLogin(AsyncPlayerPreLoginEvent event) {
+    if (Game.hasStarted()) {
+      event.disallow(Result.KICK_OTHER, ChatColor.RED + "Game Has Already Started!");
+    }
+    
+  }
+  
+}
