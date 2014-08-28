45513457138ed90c0a266a875c9a8fe176377877
TheCowJr <josh.crazymajr@gmail.com>
2014-08-27 21:52:58 -0700

    Update PlayerQuit.java

--- com/theparkmc/simonsays/listeners/player/PlayerQuit.java
+++ com/theparkmc/simonsays/listeners/player/PlayerQuit.java
@@ -1,1 +1,28 @@
-//TODO: ADD CODE
+package com.theparkmc.simonsays.listeners.player;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerQuit extends SSListener {
+  public SimonSays pl;
+  
+  public PlayerQuit (SimonSays pl) {
+    super(pl);
+  }
+  
+  @EventHandler
+  public void onPlayerQuit(PlayerQuitEvent event) {
+    if (GameState.isSate(GameState.IN_LOBBY)) {
+      Game.setCanStart(Bukkit.getOnlinePlayers().length -1 >= 4);
+      
+      Player player = event.getPlayer();
+      
+      if (Game.hasStarted()) {
+        event.setLeaveMessage(ChatColor.AQUA + player.getName() + ChatColor.GREEN + " has left " + ChatColor.DARK_GREEN + "Simon Says!");
+      }
+    }
+  }
+}
