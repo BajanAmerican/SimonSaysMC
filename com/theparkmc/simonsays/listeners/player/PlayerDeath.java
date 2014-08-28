b00e68818da544ade8965a6ea5f3e34e65eb1060
TheCowJr <josh.crazymajr@gmail.com>
2014-08-27 19:39:09 -0700

    Update PlayerDeath.java

--- com/theparkmc/simonsays/listeners/player/PlayerDeath.java
+++ com/theparkmc/simonsays/listeners/player/PlayerDeath.java
@@ -1,1 +1,20 @@
-//TODO: Add Code
+package com.theparkmc.simonsays.listeners.player;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class PlayerDeath extends SSListener {
+  
+  public PlayerDeath(SimonSays pl) {
+    super(pl);
+  }
+  
+  @EventHandler
+  public void onPlayerDeath(PlayerDeathEvent event) {
+    Player player = event.getEntity();
+    //Add Team stuff here
+  }
+  
+}
