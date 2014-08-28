c1aa005c39bebd661ce112ca3f20a0a8b9d7ce70
TheCowJr <josh.crazymajr@gmail.com>
2014-08-27 19:24:05 -0700

    Update EntityDamage.java

--- com/theparkmc/simonsays/listeners/entity/EntityDamage.java
+++ com/theparkmc/simonsays/listeners/entity/EntityDamage.java
@@ -1,1 +1,23 @@
-//TODO: Add Code
+package com.theparkmc.simonsays.listeners.entity;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class EntityDamage extends SSListener {
+  
+  public EntityDamage (SimonSays pl) {
+    super(pl);
+  }
+  
+  @EventHandler
+  public void onHit(EntityDamageByEntityEvent event) {
+    Player player = (Player) e.getEntity();
+		Player damager = (Player) e.getDamager();
+		
+    if((!(e.getEntity() instanceof Player) && e.getDamager() instanceof Player)){
+			e.setCancelled(true);
+    }
+  }
+  
+}
