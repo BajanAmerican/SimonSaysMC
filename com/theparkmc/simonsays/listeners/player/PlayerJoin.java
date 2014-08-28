8f62af5ec2071a54ab231eb5ee03ce258fab417a
TheCowJr <josh.crazymajr@gmail.com>
2014-08-27 19:45:57 -0700

    Update PlayerJoin.java

--- com/theparkmc/simonsays/listeners/player/PlayerJoin.java
+++ com/theparkmc/simonsays/listeners/player/PlayerJoin.java
@@ -1,1 +1,18 @@
-//TODO: Add Code
+package com.theparkmc.simonsays.listeners.player;
+
+//Add Imports here
+
+public class PlayerJoin extends SSListener {
+  
+  public PlayerJoin(SimonSays pl) {
+    super(pl);
+  }
+  
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent event) {
+    Player player = event.getPlayer();
+    
+    //Add More Code When given more information
+  }
+  
+}
