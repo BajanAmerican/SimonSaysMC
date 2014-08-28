799d105daa04237b4f7cb12474cd54f0e8077db2
TheCowJr <josh.crazymajr@gmail.com>
2014-08-27 22:31:58 -0700

    Update and rename Main.java to SimonSays.java

---
+++ com/theparkmc/simonsays/SimonSays.java
@@ -0,0 +1,51 @@
+package com.theparkmc.simonsays;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimonSays extends JavaPlugin {
+  
+  public static int startCountdownId;
+  
+  public void onEnable() {
+    GameState.setState(GameState.IN_LOBBY);
+    
+    registerKits();
+    startCountdown();
+    registerListeners();
+    
+    getLogger().info("SimonSays Has Been Enabled!");
+  }
+  
+  public void registerListeners() {
+    PluginManager pm = Bukkit.getServer().getPluginManager();
+    pm.registerEvents(new PlayerJoin(this), this);
+    pm.registerEvents(new PlayerDeath(this), this);
+    pm.registerEvents(new PlayerQuit(this), this);
+    pm.registerEvents(new AsyncPlayerPreLogin(this), this);
+  }
+  
+  public void startCountdown() {
+    startCountdown.timeUntilStart = 60;
+    startCountdownId = Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new startCountdown(this), 20L, 20);
+  }
+  
+  public void stopCountdown() {
+    Bukkit.getServer().getScheduler().cancelTask(startCountdownId);
+  }
+  
+  public void restartCountdown() {
+    stopCountdown();
+    startCountdown();
+  }
+  
+  public void onDisable() {
+    getLogger().info("SimonSays Has Been Disabled!");
+  }
+  
+}
