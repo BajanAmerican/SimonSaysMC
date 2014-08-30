e22b1feb5f66275c7d9edfbbe234bbc1a7640fe7
frostythedev <frostbitegraphicsyt@gmail.com>
2014-08-30 16:00:40 -0400

    Update Map.java

--- com/theparkmc/simonsays/handlers/Map.java
+++ com/theparkmc/simonsays/handlers/Map.java
@@ -1,1 +1,12 @@
-//TODO: Add Code
+package com.theparkmc.simonsays.handlers;
+
+public class Map { 
+  
+  
+  World gameworld = this.getServer().createWorld(new WorldCreator("game_world"));
+  
+  // TODO: Add teleporting to the arena once done loading
+  // TODO: Add Debugging messages for players with certain permissions
+  
+  
+}
