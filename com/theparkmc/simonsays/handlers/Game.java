1da7f40a3fee2304e51d5421011ba9c9453773cd
frostythedev <frostbitegraphicsyt@gmail.com>
2014-08-30 16:10:17 -0400

    Update Game.java

--- com/theparkmc/simonsays/handlers/Game.java
+++ com/theparkmc/simonsays/handlers/Game.java
@@ -1,1 +1,21 @@
-//REMEMBER: DO THIS LAST
+package com.theparkmc.simonsays.handlers;
+
+import com.theparkmc.simonsays.utils.GameState;
+import com.theparkmc.simonsays.handlers.Map;
+
+public class Game {  
+  
+  private String fileName;
+  
+  
+  if(GameState.getState(GameState.IN_LOBBY)){
+    Map.loadMap();
+   
+  }
+  
+  public World getWorld() {
+		return Bukkit.getWorld(fileName);
+	}
+	
+	
+}
